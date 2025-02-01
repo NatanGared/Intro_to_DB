b14a87e51ca3c27726b8b624000a3c20cc3823d4
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-01 21:03:55 +0300

    my first database code

---
+++ alx_book_store.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE alx_book_store;
+CREATE TABLE Books
+(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id (Foreign Key referencing Authors table) NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE
+);
+CREATE TABLE Authors
+(
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+CREATE TABLE Customers
+(
+    customer_id (Primary Key),
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    `address` TEXT NOT NULL
+);
+CREATE TABLE Orders
+(
+    order_id (Primary Key),
+    customer_id (Foreign Key referencing Customers table) NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE Orders_Stores
+(
+    orderdetailid (Primary Key),
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
