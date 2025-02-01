08da99d712b268d24b1d0ae6d4c7f1f13a9f8233
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-01 21:16:11 +0300

    my first database code

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,12 +26,12 @@ CREATE TABLE Orders
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-CREATE TABLE Orders_Details
+CREATE TABLE Order_Details
 (
     orderdetailid (Primary Key),
     order_id INT,
     book_id INT,
-    quantity DOUBLE
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
